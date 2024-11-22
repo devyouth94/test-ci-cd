9c6803c2bf249a6f159db2c125732729ea1ee710
devyouth94 <devyouth94@gmail.com>
2024-11-22 15:45:46 +0900

    hmm

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,8 +6,4 @@ describe("template spec", () => {
   it('should contain "Get started by ~" text', () => {
     cy.contains("Get started by editing");
   });
-
-  it('should contain "Docs" heading anchor with correct href prop', () => {
-    cy.contains("Docs").should("have.attr", "href").and("include", "https://nextjs.org/docs");
-  });
 });
